3eb692406054a7c95d2236d22a2ee2239777441f
Johann <justmelelou@gmail.com>
2020-05-18 23:00:09 +0800

    Add favicon; add date on to-do list

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,6 +1,21 @@
+//Date time
+var serverTime = new Date();
+
+function updateTime() {
+    serverTime = new Date(serverTime.getTime() + 1000);
+    $('#date-time-display').html(serverTime.toLocaleString());
+}
+
+$(function() {
+    updateTime();
+    setInterval(updateTime, 1000);
+});
+
+
+//This function adds new entries to the list
 $('button#save').click(function(){
   userInput = $('input#userInput').val();
-  inserted = '<li class="list-in">'+ userInput +'<td>'
+  inserted = '<li class="list-in">'+ userInput +'</li>'
   if(userInput){
     $(inserted).hide().appendTo('.list-body').fadeIn(500);
     $('input#userInput').val('');
